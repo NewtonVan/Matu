90f50f24cc8d809dd2a04c042c8280535b5de758
NewtonVan <48061231+NewtonVan@users.noreply.github.com>
2019-12-31 12:24:41 +0800

    Update matu466.cpp

--- matu466.cpp
+++ matu466.cpp
@@ -1,12 +1,7 @@
 	flag= 0==year%400 || (year%100 && 0== year%4) ? 1 : 0;
-	month= k= 1;
-	while (1){
-		if (daysNum> days[flag][month]){
-			daysNum-= days[flag][month];
-			++month;
-		}
-		else{
-			day= daysNum;
-			break;
-		}
+	month= 1;
+	while (daysNum> days[flag][month]){
+		daysNum-= days[flag][month];
+		++month;
 	}
+	day= daysNum;
